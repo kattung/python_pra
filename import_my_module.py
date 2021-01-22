c88ed4ea422e90d3505a7633d72c8137472e205f
Katriona Tung <kattung.tung@sifive.com>
2021-01-22 16:13:43 +0800

    pratice: import module from another path

--- import_my_module.py
+++ import_my_module.py
@@ -2,6 +2,15 @@
 
 import my_module
 
+### import customized module from different path
+import sys
+import os.path as osp
+cmd_dir = osp.join(osp.dirname(__file__), "cmd")
+sys.path.append(cmd_dir)
+import common
+
 my_module.printhello("abc")
 my_module.adder(4, 5)
 print my_module.mult(43, 5)
+
+print(common.linux_banner)
